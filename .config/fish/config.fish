6cc243be8603b33984ecf9c815b479b5d611b910
SpaceShaman <spaceshaman@tuta.io>
2023-07-04 14:23:03 +0200

    To generate a git commit message for your changes, you can use the following command:
    ```shell
    git add . && git diff --staged | sgpt "Generate git commit message, for my changes" | git commit -F - && git push --all
    ```
    
    This command adds all changes to the staging area, generates a commit message using the `sgpt` command, commits the changes with the generated message, and pushes the changes to all branches.
    
    Make sure you have the `sgpt` command installed and configured properly for generating commit messages.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,6 +4,7 @@ if status is-interactive
 	alias py='python'
 	alias pip='python -m pip'
 	alias vim=nvim
+	alias commit='git add . && git diff --staged | sgpt "Generate git commit message, for my changes" | git commit -F - && git push --all'
 	export PYTHONSTARTUP=$HOME/.config/pythonstartup
 	set PATH "$HOME/.local/bin:$PATH"
 end

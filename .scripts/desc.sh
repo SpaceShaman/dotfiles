a2e843925cea0d803193144b7abf51570d43e1f0
SpaceShaman <spaceshaman@tuta.io>
2023-07-04 16:34:37 +0200

    The provided diff shows changes made to several files in a Git repository. Here is a summary of the changes:
    1. In the file `.scripts/desc.sh`, the command `git log --oneline --pretty=%s%n%b "$commit_range"` is modified to pipe the output to `sgpt` command. The purpose of this modification is to create a version description according to the rules outlined in 'Keep a Changelog' and return only a description of the changes without any additional information. The resulting description is then used as the commit message for a new Git commit.
    
    2. In the file `.scripts/release.sh`, the variable `tag_description` is assigned the output of the modified `git log` command mentioned above. This variable is then used as the tag message when creating a new Git tag with the command `git tag $new_version -m "$tag_description"`.
    
    3. In the file `README.md`, the line "My dotfiles for my Arch Linux setup." is modified to "My dotfiles for Ubuntu 22.04 LTS.".
    
    4. The file `requirements.txt` is deleted.
    
    5. The file `test.sh` is deleted.
    
    Please let me know if you need any further assistance.

--- .scripts/desc.sh
+++ .scripts/desc.sh
@@ -3,4 +3,4 @@
 latest_tag=$(git tag | sort -V | tail -n 1)
 commit_range="$latest_tag..HEAD"
 
-git log --oneline --pretty=%s%n%b "$commit_range"
+git log --oneline --pretty=%s%n%b "$commit_range" | sgpt "Create version description according to the rules outlined in 'Keep a Changelog', return only a description of the changes without any additional information. " | git commit -F -

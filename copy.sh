ce461ab6249473c42bef24c0027064f0d68fde6e
SpaceShaman <spaceshaman@tuta.io>
2023-07-05 12:54:34 +0200

    Refactor copy.sh script to use a more concise and efficient method for copying files and directories.

--- copy.sh
+++ copy.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-cp -r .config/ /home/$SUDO_USER/
-cp -r .fonts/ /home/$SUDO_USER/
-cp -r .moc/ /home/$SUDO_USER/
-cp -r .scripts/ /home/$SUDO_USER/
+cp -R .[^.]* /home/$SUDO_USER/
 chmod 755 /home/$SUDO_USER/.moc/config

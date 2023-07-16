03cb339e2ca5e6382d3a79eefd5fabcf474a5508
SpaceShaman <spaceshaman@tuta.io>
2023-07-16 19:34:28 +0200

    "Updated commit.sh script to use hidden .commit file instead of visible commit file"

--- .scripts/commit.sh
+++ .scripts/commit.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-touch commit
+touch .commit
 git add -A
-git diff --staged | sgpt --model "gpt-4" "Generate git commit message, for my changes:\n\n" > commit
-nano commit
-git commit -F commit
+git diff --staged | sgpt --model "gpt-4" "Generate git commit message, for my changes:\n\n" > .commit
+nano .commit
+git commit -F .commit
 git push --all
-rm commit
+rm .commit

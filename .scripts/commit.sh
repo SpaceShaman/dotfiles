4dfc873d33bc3b46f792d02ee6dd410e577afded
SpaceShaman <spaceshaman@tuta.io>
2023-07-16 20:48:17 +0200

    The changes made in the commit.sh script involve replacing the vim editor with the nvim editor for editing the commit message.
    In the release.sh script, several changes have been made. The vim editor has been replaced with the nvim editor for editing the release message. The commented out lines of code for checking out the master branch, merging the develop branch into master, tagging the new version, pushing all changes and tags, checking out the develop branch again, and removing the .release file have been uncommented, enabling these actions. The lines of code for printing the last version and new version have also been uncommented.

--- .scripts/commit.sh
+++ .scripts/commit.sh
@@ -3,7 +3,7 @@
 git add -A
 touch .commit
 git diff --staged | sgpt --model "gpt-4" "Generate git commit message, for my changes:\n\n" > .commit
-vim .commit
+nvim .commit
 git commit -F .commit --no-verify
 git push --all
 rm .commit

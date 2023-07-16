2d90927f04d6e89920c696842a633145aa02c222
SpaceShaman <spaceshaman@tuta.io>
2023-07-16 19:48:45 +0200

    The commit script has been updated to skip the pre-commit and commit-msg hooks by adding the --no-verify option to the git commit command. This allows the commit to proceed without any checks that might be in place.

--- .scripts/commit.sh
+++ .scripts/commit.sh
@@ -4,6 +4,6 @@ git add -A
 touch .commit
 git diff --staged | sgpt --model "gpt-4" "Generate git commit message, for my changes:\n\n" > .commit
 nano .commit
-git commit -F .commit
+git commit -F .commit --no-verify
 git push --all
 rm .commit

3d5eb8c3cb28c90c1ca2c24df26f532963b0372c
SpaceShaman <spaceshaman@tuta.io>
2023-09-07 08:41:12 +0200

    Updated wifi.sh script to include forget network functionality and improved argument handling

--- .scripts/wifi.sh
+++ .scripts/wifi.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-if [ $# -ne 1 ]; then
+if [ $# -eq 0 ]; then
     echo -e "\033[1;31mUse: wifi <wifi name> to connect to a wifi network.\033[0m"
     echo -e "\033[1;31mUse: wifi -l or --list to list available wifi networks.\033[0m"
     echo -e "\033[1;31mUse: wifi -d or --disconnect to disconnect from wifi.\033[0m"
+    echo -e "\033[1;31mUse: wifi <wifi name> -f or --forget to forget a wifi network.\033[0m"
     exit 1
 fi
 
@@ -14,7 +15,11 @@ fi
 
 if [ $1 = "-d" ] || [ $1 = "--disconnect" ]; then
     nmcli dev disconnect wlo1
-    echo -e "\033[1;32mDisconnected from wifi.\033[0m"
+    exit 1
+fi
+
+if [ $# -eq 2 ] && ([ $2 = "-f" ] || [ $2 = "--forget" ]); then
+    nmcli connection delete $1
     exit 1
 fi
 

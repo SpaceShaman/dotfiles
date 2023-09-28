53c747fe2ee401226f37c097a78e74ba74abbe22
SpaceShaman <spaceshaman@tuta.io>
2023-09-28 16:28:49 +0200

    fix(setup.sh): replace chrome-gnome-shell with gnome-shell-pomodoro in apt install command to install the correct gnome shell extension

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ apt-add-repository ppa:fish-shell/release-3
 apt-add-repository ppa:deadsnakes/ppa
 add-apt-repository ppa:neovim-ppa/stable
 apt update
-apt install -y fish curl neovim git wget htop moc alacritty mc nodejs npm python3.11 python3.11-dev python3.11-venv python3-pip python3.11-distutils python3.11-lib2to3 python3.11-gdbm python3.11-tk chrome-gnome-shell
+apt install -y fish curl neovim git wget htop moc alacritty mc nodejs npm python3.11 python3.11-dev python3.11-venv python3-pip python3.11-distutils python3.11-lib2to3 python3.11-gdbm python3.11-tk gnome-shell-pomodoro 
 
 # Set python3.11 as default python version and pip
 update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.11 1

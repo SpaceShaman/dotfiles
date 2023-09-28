eff90feb1a8968ee46e380f51114f0279e0081c6
SpaceShaman <spaceshaman@tuta.io>
2023-09-28 19:10:04 +0200

    feat(break.sh): enhance break notification script for better user experience
    - Add sleep command to delay the execution of the script
    - Calculate row and column for center alignment of the text
    - Clear terminal before displaying the message
    - Display the break message in green color for better visibility
    - Sleep for 5 seconds after displaying the message to give user enough time to read it

--- .scripts/break.sh
+++ .scripts/break.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
-# Script used in pomodoro timer to notify user about long break time
-echo "PORA NA DŁUGĄ PRZERWĘ!"
+# Script to display a message about a break to use in pomodoro timer
+sleep 0.1
+text="PORA NA DŁUGĄ PRZERWĘ!"
+row=$(( $(tput lines) / 2 - 1))
+col=$(( ($(tput cols) - ${#text}) / 2))
+tput clear
+tput cup $row $col
+
+echo -e "\033[1;32m$text\033[0m"
+
 sleep 5

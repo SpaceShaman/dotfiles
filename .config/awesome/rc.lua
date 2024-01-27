c8310346561b59f2363c987b035c2b1f052a11d0
SpaceShaman <spaceshaman@tuta.io>
2024-01-27 14:36:30 +0100

    fix(rc.lua): update screen property values from numeric index to screen name for better readability and maintainability feat(rc.lua): add support for opening Spotify on screen "eDP-1" and tag "1" and Signal on screen "eDP-1" and tag "2"

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -555,17 +555,17 @@ awful.rules.rules = {
         properties = { titlebars_enabled = false }
     },
 
-    -- Open on specific tag
+    -- Open on specific tag and screen
     {
         rule = { class = "Spotify" },
-        properties = { screen = 2, tag = "1", switchtotag = true },
+        properties = { screen = "eDP-1", tag = "1", switchtotag = true },
         callback = function(c)
             awful.spawn.with_shell("playerctl play-pause");
         end
     },
     {
         rule = { class = "Signal" },
-        properties = { screen = 2, tag = "2", switchtotag = true },
+        properties = { screen = "eDP-1", tag = "2", switchtotag = true },
     },
 }
 -- }}}

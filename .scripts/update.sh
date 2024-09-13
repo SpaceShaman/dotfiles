7f6490511a9a68ec456bfbf65bf97ae5d5ea58f2
SpaceShaman <spaceshaman@tuta.io>
2024-09-13 07:39:22 +0200

    🔧 (update.sh): update app_name check to match 'connect' instead of 'ql_connect' 🔧 (update.sh): add conditional logic for 'connect' app to handle specific docker-compose commands

--- .scripts/update.sh
+++ .scripts/update.sh
@@ -6,10 +6,14 @@ app_name=$(basename $(pwd))
 if [ "$app_name" = "app" ]; then
     app_name=$(basename $(dirname $(pwd)))
 fi
-if [ "$app_name" != "holidays" ] && [ "$app_name" != "warehouse" ] && [ "$app_name" != "ql_connect" ] && [ "$app_name" != "analytics" ]; then
+if [ "$app_name" != "holidays" ] && [ "$app_name" != "warehouse" ] && [ "$app_name" != "connect" ] && [ "$app_name" != "analytics" ]; then
     echo -e "\033[1;31mYou are not in holidays, warehouse or ql_connect directory.\033[0m"
     exit 1
 fi
 
-ssh krzysztofs@146.59.126.189 "cd $app_name && git pull && docker-compose down && docker-compose build && docker-compose up -d"
+if [ "$app_name" != "connect" ]; then
+    ssh krzysztofs@146.59.126.189 "cd core/connect && git pull && cd .. && docker-compose down connect && docker-compose build connect && docker-compose up -d connect"
+else
+    ssh krzysztofs@146.59.126.189 "cd $app_name && git pull && docker-compose down && docker-compose build && docker-compose up -d"
+fi
 echo -e "\033[1;32mUpdate to the new version done.\033[0m"

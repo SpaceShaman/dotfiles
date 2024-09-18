73011b47231267f627c6ad472b39c404626edb75
SpaceShaman <spaceshaman@tuta.io>
2024-09-18 09:29:10 +0200

    🔧 (update.sh): add support for 'frontend' app in update script ♻️ (update.sh): refactor directory check and update command for better clarity

--- .scripts/update.sh
+++ .scripts/update.sh
@@ -6,13 +6,13 @@ app_name=$(basename $(pwd))
 if [ "$app_name" = "app" ]; then
     app_name=$(basename $(dirname $(pwd)))
 fi
-if [ "$app_name" != "holidays" ] && [ "$app_name" != "warehouse" ] && [ "$app_name" != "connect" ] && [ "$app_name" != "analytics" ]; then
-    echo -e "\033[1;31mYou are not in holidays, warehouse or ql_connect directory.\033[0m"
+if [ "$app_name" != "holidays" ] && [ "$app_name" != "warehouse" ] && [ "$app_name" != "connect" ] && [ "$app_name" != "analytics" ] && [ "$app_name" != "frontend" ]; then
+    echo -e "\033[1;31mYou are not in the correct directory to update the app.\033[0m"
     exit 1
 fi
 
-if [ "$app_name" = "connect" ]; then
-    ssh krzysztofs@146.59.126.189 "cd core/connect && git pull && cd .. && docker-compose down connect && docker-compose build connect && docker-compose up -d connect"
+if [ "$app_name" = "connect" ] || [ "$app_name" = "frontend" ]; then
+    ssh krzysztofs@146.59.126.189 "cd core/$app_name && git pull && cd .. && docker-compose down $app_name && docker-compose build $app_name && docker-compose up -d $app_name"
 else
     ssh krzysztofs@146.59.126.189 "cd $app_name && git pull && docker-compose down && docker-compose build && docker-compose up -d"
 fi

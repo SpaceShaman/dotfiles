57bee26a18ef10ed479c867ac5b5d5dbec543fb0
SpaceShaman <spaceshaman@tuta.io>
2024-10-10 17:08:41 +0200

    ✨ (install_usb_hdmi_drivers.sh): add script to install USB HDMI drivers
    🔧 (setup.sh): remove pomotroid from default package installation list

---
+++ install_usb_hdmi_drivers.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Download the Ubuntu APT package provided by Synaptics.com, the official 
+# makers of DisplayLink
+wget https://www.synaptics.com/sites/default/files/Ubuntu/pool/stable/main/all/synaptics-repository-keyring.deb
+
+# Install the DisplayLink APT package keyring
+sudo apt install ./synaptics-repository-keyring.deb
+
+# Update your APT package cache
+sudo apt update
+
+# Install the DisplayLink driver provided by Synaptics.com, the official source
+sudo apt install displaylink-driver

9ab2f305ec16979dd9c76974b070fc7e6c5e5e3b
SpaceShaman <spaceshaman@tuta.io>
2024-10-21 16:30:47 +0200

    🔧 (screenshot.sh): change screenshot format to PNG and copy to clipboard
    🔧 (setup.sh): add xclip to installation for clipboard support

--- .scripts/screenshot.sh
+++ .scripts/screenshot.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-scrot -s '/home/ton618/Obrazy/screen-%Y-%m-%d-%H_%M.jpg'
+# scrot -s '/home/ton618/Obrazy/screen-%Y-%m-%d-%H_%M.jpg'
+scrot -s '/home/ton618/Obrazy/screen-%Y-%m-%d-%H_%M.png' -e 'xclip -selection clipboard -t image/png -i $f'

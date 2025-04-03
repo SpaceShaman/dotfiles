9a07c51508dd3aa31b6cbbe65d9f746ea20ea3f7
SpaceShaman <spaceshaman@tuta.io>
2025-04-03 17:23:37 +0200

    🔧 (theme.sh): remove unused GTK 1.0, 2.0, and 4.0 settings files to simplify configuration ♻️ (theme.sh): update theme switching logic to use GTK 3.0 settings only and improve maintainability ✨ (theme.sh): add system GTK theme change command for both light and dark modes to ensure consistency across applications

--- .scripts/theme.sh
+++ .scripts/theme.sh
@@ -12,11 +12,10 @@ theme=$(cat ~/.config/theme)
 if [ "$theme" = "dark" ]; then
     # change system theme to light
     gsettings set org.gnome.desktop.interface color-scheme 'default'
+    gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita'
     # change gtk theme to light
-    sed -i 's/Adwaita-dark/Adwaita/' ~/.config/gtk-1.0/settings.ini
-    sed -i 's/Adwaita-dark/Adwaita/' ~/.config/gtk-2.0/settings.ini
     sed -i 's/Adwaita-dark/Adwaita/' ~/.config/gtk-3.0/settings.ini
-    sed -i 's/Adwaita-dark/Adwaita/' ~/.config/gtk-4.0/settings.ini
+    sed -i 's/gtk-application-prefer-dark-theme=1/gtk-application-prefer-dark-theme=0/' ~/.config/gtk-3.0/settings.ini
     # change theme to light in alacritty
     sed -i 's/dark/light/' ~/.config/alacritty/alacritty.yml
     # change rofi theme to light
@@ -25,18 +24,16 @@ if [ "$theme" = "dark" ]; then
     sed -i 's/"workbench.colorTheme": "Ayu Dark Bordered"/"workbench.colorTheme": "Ayu Light Bordered"/g' ~/.config/Code/User/settings.json
     # change theme to light in awesome
     sed -i 's/dark.lua/light.lua/' ~/.config/awesome/rc.lua
-    awesome-client 'awesome.restart()'
 
     echo "light" > ~/.config/theme
     theme="light"
 else
-    # change system theme to dark
+    # # change system theme to dark
     gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+    gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark'
     # change gtk theme to dark
-    sed -i 's/Adwaita/Adwaita-dark/' ~/.config/gtk-1.0/settings.ini
-    sed -i 's/Adwaita/Adwaita-dark/' ~/.config/gtk-2.0/settings.ini
     sed -i 's/Adwaita/Adwaita-dark/' ~/.config/gtk-3.0/settings.ini
-    sed -i 's/Adwaita/Adwaita-dark/' ~/.config/gtk-4.0/settings.ini
+    sed -i 's/gtk-application-prefer-dark-theme=0/gtk-application-prefer-dark-theme=1/' ~/.config/gtk-3.0/settings.ini
     # change alacritty theme to dark
     sed -i 's/light/dark/' ~/.config/alacritty/alacritty.yml
     # change rofi theme to dark
@@ -45,10 +42,10 @@ else
     sed -i 's/"workbench.colorTheme": "Ayu Light Bordered"/"workbench.colorTheme": "Ayu Dark Bordered"/g' ~/.config/Code/User/settings.json
     # change theme to dark in awesome
     sed -i 's/light.lua/dark.lua/' ~/.config/awesome/rc.lua
-    awesome-client 'awesome.restart()'
 
     echo "dark" > ~/.config/theme
     theme="dark"
 fi
 
 echo -e "\033[1;32mTheme changed to: $theme\033[0m"
+awesome-client 'awesome.restart()'

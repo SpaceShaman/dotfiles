7031cc4fab9fdb11665645ebe869adfa3246eb9e
SpaceShaman <spaceshaman@tuta.io>
2025-07-13 18:49:33 +0200

    ⬆️ (setup.sh): add ripgrep to the list of installed packages to enhance search capabilities

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ apt install -y software-properties-common
 apt-add-repository -y ppa:fish-shell/release-3 
 apt-add-repository -y ppa:neovim-ppa/stable
 apt-add-repository -y ppa:aslatter/ppa
-apt install -y fish curl neovim git wget htop moc alacritty mc nodejs npm awesome compton rofi light pavucontrol playerctl scrot xclip translate-shell
+apt install -y fish curl neovim git wget htop moc alacritty mc nodejs npm awesome compton rofi light pavucontrol playerctl scrot xclip translate-shell ripgrep
 
 # Set neovim as default editor
 update-alternatives --install /usr/bin/vi vi /usr/bin/nvim 60

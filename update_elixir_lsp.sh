fd66d08b23cbe07a5fce6a5712e599722d2d64f1
SpaceShaman <spaceshaman@tuta.io>
2025-08-03 11:45:52 +0200

    ✨ (update_elixir_lsp.sh): add colored output message to indicate successful update of Elixir LSP

--- update_elixir_lsp.sh
+++ update_elixir_lsp.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-# Download the latest release of the elixir-lsp
+BOLD='\033[1m'
+GREEN='\033[32m'
+RESET='\033[0m'
+
 LATEST_URL=$(curl -s https://api.github.com/repos/elixir-lsp/elixir-ls/releases/latest \
   | jq -r '.assets[] | select(.name | test("elixir-ls.*\\.zip$")).browser_download_url')
 curl -L -o elixir-ls.zip $LATEST_URL
 
-# Delete the old release
 rm -rf /home/ton618/.local/share/elixir-lsp/elixir-ls
 
-# Create the directory if it doesn't exist
 mkdir -p /home/ton618/.local/share/elixir-lsp
 
-# Extract the new release
 unzip elixir-ls.zip -d /home/ton618/.local/share/elixir-lsp/elixir-ls
 
-# Delete the zip file
 rm elixir-ls.zip
+
+echo -e "${BOLD}${GREEN}Elixir LSP is updated.${RESET}"

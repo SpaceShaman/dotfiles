62f51b863a35d4693ef7e2c5ad8a50ab14edfcd1
SpaceShaman <spaceshaman@tuta.io>
2025-08-21 17:03:05 +0200

    🔧 (sync_dotfiles_with_system.sh): update stow command to adopt existing files and change directory to parent before execution

--- setup_scripts/sync_dotfiles_with_system.sh
+++ setup_scripts/sync_dotfiles_with_system.sh
@@ -6,5 +6,6 @@ YELLOW='\033[33m'
 RESET='\033[0m'
 
 echo -e "${BOLD}${YELLOW}Syncing dotfiles with system via stow...${RESET}"
-stow -t ~ -d ~/dotfiles
+cd ..
+stow --adopt .
 echo -e "${BOLD}${GREEN}Dotfiles synced.${RESET}"

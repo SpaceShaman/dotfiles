6825c33994c2f8b1b9a5593b187bde259826d41f
SpaceShaman <spaceshaman@tuta.io>
2025-08-21 16:39:17 +0200

    🔧 (install_apt_packages.sh): comment out fish repository addition to prevent potential conflicts or issues with existing package sources

--- setup_scripts/install_apt_packages.sh
+++ setup_scripts/install_apt_packages.sh
@@ -11,8 +11,8 @@ apt update && apt upgrade -y
 apt install -y curl
 
 # Add fish repo
-echo 'deb http://download.opensuse.org/repositories/shells:/fish:/release:/4/Debian_13/ /' | sudo tee /etc/apt/sources.list.d/shells:fish:release:4.list
-curl -fsSL https://download.opensuse.org/repositories/shells:fish:release:4/Debian_13/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/shells_fish_release_4.gpg > /dev/null
+# echo 'deb http://download.opensuse.org/repositories/shells:/fish:/release:/4/Debian_13/ /' | sudo tee /etc/apt/sources.list.d/shells:fish:release:4.list
+# curl -fsSL https://download.opensuse.org/repositories/shells:fish:release:4/Debian_13/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/shells_fish_release_4.gpg > /dev/null
 
 apt install -y fish git wget htop moc alacritty mc nodejs npm awesome compton rofi light pavucontrol playerctl scrot xclip translate-shell ripgrep fd-find bat stow rsync
 

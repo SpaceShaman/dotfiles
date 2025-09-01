9964569e4a2b29f988b63d16b58ddeddba62cdca
SpaceShaman <spaceshaman@tuta.io>
2025-09-01 09:03:41 +0200

    🔧 (config.fish): fix indentation for better readability and consistency

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,34 +1,34 @@
 if status is-interactive
-	set fish_greeting
+  set fish_greeting
   fzf_configure_bindings --variables=\e\cv --directory=\cf
   set --universal pure_enable_single_line_prompt true
-	set PATH "$HOME/.local/bin:$PATH"
-	alias g=sgpt
-	alias c=oco
-	alias w=wifi
-	alias p=python
-	alias v=nvim
-	alias vim=nvim
+  set PATH "$HOME/.local/bin:$PATH"
+  alias g=sgpt
+  alias c=oco
+  alias w=wifi
+  alias p=python
+  alias v=nvim
+  alias vim=nvim
   alias cat=batcat
   alias l=lazygit
   alias cp='rsync -ah --info=progress2'
   alias mv='rsync -ah --info=progress2 --remove-source-files'
-	alias t='trans -b :en'
+  alias t='trans -b :en'
   alias tp='trans -b :pl'
-	alias s=screen
-	alias co='code .; exit'
-	alias release='bash ~/.scripts/release.sh'
-	alias update='bash ~/.scripts/update.sh'
-	alias theme='bash ~/.scripts/theme.sh'
-	alias wifi='bash ~/.scripts/wifi.sh'
-	alias screen='bash ~/.scripts/screen.sh'
-	export PYTHONSTARTUP=$HOME/.config/pythonstartup
-	export PATH="$PATH:/opt/nvim/"
+  alias s=screen
+  alias co='code .; exit'
+  alias release='bash ~/.scripts/release.sh'
+  alias update='bash ~/.scripts/update.sh'
+  alias theme='bash ~/.scripts/theme.sh'
+  alias wifi='bash ~/.scripts/wifi.sh'
+  alias screen='bash ~/.scripts/screen.sh'
+  export PYTHONSTARTUP=$HOME/.config/pythonstartup
+  export PATH="$PATH:/opt/nvim/"
   export ASDF_DATA_DIR="$HOME/.local/share/asdf"
   export PATH="$ASDF_DATA_DIR/shims:$PATH"
-	export EDITOR=nvim
-	export TERM=xterm-256color
-	pyenv init - | source
+  export EDITOR=nvim
+  export TERM=xterm-256color
+  pyenv init - | source
   autopy
   autoenv
 end

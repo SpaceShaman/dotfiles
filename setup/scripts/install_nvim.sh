a82bce4f1c476abc57b6a21361eccb899e3020c9
SpaceShaman <spaceshaman@tuta.io>
2025-09-04 19:12:25 +0200

    🔧 (install_nvim.sh): add sudo to ensure script has necessary permissions for directory operations

--- setup/scripts/install_nvim.sh
+++ setup/scripts/install_nvim.sh
@@ -1,9 +1,9 @@
 if ask "Do you want to install Neovim?"; then
-  rm -rf /opt/nvim
+  sudo rm -rf /opt/nvim
   curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.appimage
   chmod u+x nvim-linux-x86_64.appimage
-  mkdir -p /opt/nvim
-  mv nvim-linux-x86_64.appimage /opt/nvim/nvim
+  sudo mkdir -p /opt/nvim
+  sudo mv nvim-linux-x86_64.appimage /opt/nvim/nvim
 
   update-alternatives --install /usr/bin/vi vi /usr/bin/nvim 60
   update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 60

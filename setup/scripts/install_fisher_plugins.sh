bf8c7d8cfb579f426752ca6fbdca5cbb5c389ae9
SpaceShaman <spaceshaman@tuta.io>
2025-09-04 20:19:00 +0200

    ✨ (install_fisher_plugins.sh): add user prompt for Fisher plugin installation
    🔧 (setup.sh): include Fisher plugin installation script in setup process

--- setup/scripts/install_fisher_plugins.sh
+++ setup/scripts/install_fisher_plugins.sh
@@ -1,13 +1,5 @@
-#!/bin/bash
-
-BOLD='\033[1m'
-GREEN='\033[32m'
-YELLOW='\033[33m'
-RESET='\033[0m'
-
-echo -e "${BOLD}${YELLOW}Installing fisher plugins...${RESET}"
-
-chown -R $USER ~/.config/fish
-fish -c "fisher update"
-
-echo -e "${BOLD}${GREEN}Fisher plugins installed.${RESET}"
+if ask "Do you want to install Fisher plugins?"; then
+  chown -R $USER ~/.config/fish
+  fish -c "fisher update"
+  success "Fisher plugins installed!"
+fi
--- setup/setup.sh
+++ setup/setup.sh
@@ -9,6 +9,7 @@ info "Starting setup..."
 . scripts/install_nvim.sh
 . scripts/install_docker.sh
 . scripts/install_lazygit.sh
+. scripts/install_fisher_plugins.sh
 # . scripts/install_translate-shell.sh
 # . scripts/install_poetry_and_pyenv.sh
 # . scripts/install_uv.sh
@@ -16,6 +17,5 @@ info "Starting setup..."
 # . scripts/install_elixir.sh
 # . scripts/install_elixir_lsp.sh
 # . scripts/update_node_and_install_npm_packages.sh
-# . scripts/install_fisher_plugins.sh
 
 success "Setup complete!"

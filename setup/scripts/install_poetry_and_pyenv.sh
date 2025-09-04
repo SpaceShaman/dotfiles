f6e367527985eaefbe17683aaa7d3b6a79e42faf
SpaceShaman <spaceshaman@tuta.io>
2025-09-04 20:47:02 +0200

    🔧 (install_poetry_and_pyenv.sh): remove existing pyenv directory before installation to ensure a clean setup

--- setup/scripts/install_poetry_and_pyenv.sh
+++ setup/scripts/install_poetry_and_pyenv.sh
@@ -4,6 +4,7 @@ if ask "Do you want to install Poetry and Pyenv?"; then
       libbz2-dev libreadline-dev libsqlite3-dev curl git \
       libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
 
+  sudo rm -rf $HOME/.pyenv
   curl -fsSL https://pyenv.run | bash
 
   sudo set -Ux PYENV_ROOT $HOME/.pyenv

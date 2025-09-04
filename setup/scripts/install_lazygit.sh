75760c414e6e2e75fc42ba44a426ea5c459f732a
SpaceShaman <spaceshaman@tuta.io>
2025-09-04 20:10:38 +0200

    🔧 (install_lazygit.sh): add sudo to install command for permission elevation

--- setup/scripts/install_lazygit.sh
+++ setup/scripts/install_lazygit.sh
@@ -2,7 +2,7 @@ if ask "Do you want to install lazygit?"; then
   LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
   curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
   tar xf lazygit.tar.gz lazygit
-  install lazygit -D -t /usr/local/bin/
+  sudo install lazygit -D -t /usr/local/bin/
   rm lazygit.tar.gz lazygit
   success "Lazygit installed!"
 fi

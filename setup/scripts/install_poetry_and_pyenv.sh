86bf3dccc1b46b0eab68b444479ae6ac69a3b36a
SpaceShaman <spaceshaman@tuta.io>
2025-09-04 20:28:53 +0200

    🔧 (install_poetry_and_pyenv.sh): add user prompt for installation ♻️ (install_poetry_and_pyenv.sh): remove redundant echo statements 🔧 (setup.sh): enable poetry and pyenv installation script

--- setup/scripts/install_poetry_and_pyenv.sh
+++ setup/scripts/install_poetry_and_pyenv.sh
@@ -1,22 +1,15 @@
-#!/bin/bash
+if ask "Do you want to install Poetry and Pyenv?"; then
+  sudo apt update
+  sudo apt install -y build-essential libssl-dev zlib1g-dev \
+      libbz2-dev libreadline-dev libsqlite3-dev curl git \
+      libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
 
-BOLD='\033[1m'
-GREEN='\033[32m'
-YELLOW='\033[33m'
-RESET='\033[0m'
+  curl https://pyenv.run | bash
 
-echo -e "${BOLD}${YELLOW}Installing Poetry and Pyenv...${RESET}"
+  set -Ux PYENV_ROOT $HOME/.pyenv
+  fish_add_path $PYENV_ROOT/bin
 
-apt update
-apt install -y build-essential libssl-dev zlib1g-dev \
-    libbz2-dev libreadline-dev libsqlite3-dev curl git \
-    libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+  curl -sSL https://install.python-poetry.org | python3 -
 
-curl https://pyenv.run | bash
-
-set -Ux PYENV_ROOT $HOME/.pyenv
-fish_add_path $PYENV_ROOT/bin
-
-curl -sSL https://install.python-poetry.org | python3 -
-
-echo -e "${BOLD}${GREEN}Poetry and Pyenv installed.${RESET}"
+  success "Poetry and Pyenv installed!"
+fi

05964efa4782b9f06c6115c69412511cd422cbd8
SpaceShaman <spaceshaman@tuta.io>
2025-09-04 18:20:06 +0200

    ♻️ (install_usb_hdmi_drivers.sh): switch to using AdnanHodzic's displaylink-debian script for driver installation

--- setup/scripts/install_usb_hdmi_drivers.sh
+++ setup/scripts/install_usb_hdmi_drivers.sh
@@ -1,13 +1,8 @@
 info "Installing DisplayLink USB HDMI drivers..."
 
-# Download the Ubuntu APT package provided by Synaptics.com, the official 
-# makers of DisplayLink
-wget https://www.synaptics.com/sites/default/files/Ubuntu/pool/stable/main/all/synaptics-repository-keyring.deb
-# Install the DisplayLink APT package keyring
-sudo apt install -y ./synaptics-repository-keyring.deb
-sudo apt update -y
-# Install the DisplayLink driver provided by Synaptics.com, the official source
-sudo apt install -y displaylink-driver
-rm ./synaptics-repository-keyring.deb
+git clone https://github.com/AdnanHodzic/displaylink-debian.git
+cd displaylink-debian
+sudo ./displaylink-debian.sh
+rm -rf displaylink-debian
 
 success "DisplayLink USB HDMI drivers installed!"

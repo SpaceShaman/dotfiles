b2ef7d708c7af0de5d2b084def6210c4044cfa4c
SpaceShaman <spaceshaman@tuta.io>
2025-09-04 19:15:23 +0200

    🐛 (install_nvim.sh): fix path for update-alternatives to point to correct nvim location

--- setup/scripts/install_nvim.sh
+++ setup/scripts/install_nvim.sh
@@ -5,9 +5,9 @@ if ask "Do you want to install Neovim?"; then
   sudo mkdir -p /opt/nvim
   sudo mv nvim-linux-x86_64.appimage /opt/nvim/nvim
 
-  update-alternatives --install /usr/bin/vi vi /usr/bin/nvim 60
-  update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 60
-  update-alternatives --install /usr/bin/editor editor /usr/bin/nvim 60
+  update-alternatives --install /usr/bin/vi vi /opt/nvim/nvim 60
+  update-alternatives --install /usr/bin/vim vim /opt/nvim/nvim 60
+  update-alternatives --install /usr/bin/editor editor /opt/nvim/nvim 60
 
   echo -e "${BOLD}${GREEN}Neovim installed.${RESET}"
 fi

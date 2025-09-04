e503ea35d4f714ff294d973fd3353187458e8b72
SpaceShaman <spaceshaman@tuta.io>
2025-09-04 20:09:11 +0200

    ✨ (install_lazygit.sh): add user prompt for lazygit installation
    🔧 (setup.sh): enable lazygit installation script in setup process

--- setup/setup.sh
+++ setup/setup.sh
@@ -8,8 +8,8 @@ info "Starting setup..."
 . scripts/install_displaylink_drivers.sh
 . scripts/install_nvim.sh
 . scripts/install_docker.sh
+. scripts/install_lazygit.sh
 # . scripts/install_translate-shell.sh
-# . scripts/install_lazygit.sh
 # . scripts/install_poetry_and_pyenv.sh
 # . scripts/install_uv.sh
 # . scripts/install_asdf.sh

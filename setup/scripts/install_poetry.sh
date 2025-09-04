ee475fc9ae6752f354c5b6b2d829ced5b1ca51b6
SpaceShaman <spaceshaman@tuta.io>
2025-09-04 22:05:13 +0200

    🔧 (install_poetry.sh): remove specific Python version dependency and set POETRY_HOME for installation

--- setup/scripts/install_poetry.sh
+++ setup/scripts/install_poetry.sh
@@ -1,5 +1,4 @@
 if ask "Do you want to install Poetry?"; then
-  pyenv install 3.12
-  curl -sSL https://install.python-poetry.org | python3.12 -
+  curl -sSL https://install.python-poetry.org | POETRY_HOME=$HOME/.local/share/pypoetry sudo python3 -
   success "Poetry installed!"
 fi

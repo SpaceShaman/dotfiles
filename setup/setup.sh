c507ffa12b57a38264a7705b9a059017b1aea32a
SpaceShaman <spaceshaman@tuta.io>
2025-09-04 17:39:50 +0200

    🔧 (setup): rename setup_scripts to setup and update ignore file ♻️ (setup): change setup.sh script permissions to executable ♻️ (setup): comment out script executions in setup.sh 🔥 (setup): remove individual setup scripts and vimium options file

--- setup/setup.sh
+++ setup/setup.sh
@@ -4,18 +4,18 @@ source lib/common.sh
 info "Starting setup..."
 
 . scripts/install_apt_packages.sh
-. scripts/sync_dotfiles_with_system.sh
-. scripts/install_translate-shell.sh
-. scripts/install_usb_hdmi_drivers.sh
-. scripts/install_nvim.sh
-. scripts/install_docker.sh
-. scripts/install_lazygit.sh
-. scripts/install_poetry_and_pyenv.sh
-. scripts/install_uv.sh
-. scripts/install_asdf.sh
-. scripts/install_elixir.sh
-. scripts/install_elixir_lsp.sh
-. scripts/update_node_and_install_npm_packages.sh
-. scripts/install_fisher_plugins.sh
+# . scripts/sync_dotfiles_with_system.sh
+# . scripts/install_translate-shell.sh
+# . scripts/install_usb_hdmi_drivers.sh
+# . scripts/install_nvim.sh
+# . scripts/install_docker.sh
+# . scripts/install_lazygit.sh
+# . scripts/install_poetry_and_pyenv.sh
+# . scripts/install_uv.sh
+# . scripts/install_asdf.sh
+# . scripts/install_elixir.sh
+# . scripts/install_elixir_lsp.sh
+# . scripts/update_node_and_install_npm_packages.sh
+# . scripts/install_fisher_plugins.sh
 
 success "Setup complete!"

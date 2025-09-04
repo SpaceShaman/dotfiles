1b58c4af8d0606cad40918baf939da402852b036
SpaceShaman <spaceshaman@tuta.io>
2025-09-04 20:22:39 +0200

    🐛 (setup_fish.sh): add sudo to chown command to ensure proper permission change

--- setup/scripts/setup_fish.sh
+++ setup/scripts/setup_fish.sh
@@ -1,5 +1,5 @@
 if ask "Do you want to setup Fish?"; then
-  chown -R $USER ~/.config/fish
+  sudo chown -R $USER ~/.config/fish
   fish -c "fisher update"
   chsh -s $(which fish)
   success "Fish setup complete!"

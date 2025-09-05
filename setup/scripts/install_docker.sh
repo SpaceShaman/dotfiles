a6e75a7a32b3872725c27c1db3ab8b4f851ec0d0
SpaceShaman <spaceshaman@tuta.io>
2025-09-05 13:02:16 +0200

    🔧 (install_docker.sh): add sudo to tee command for writing to docker.list

--- setup/scripts/install_docker.sh
+++ setup/scripts/install_docker.sh
@@ -11,7 +11,7 @@ if ask "Do you want to install Docker?"; then
   sudo echo \
     "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
     $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-    tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
   sudo apt update
 
   # Install the Docker packages

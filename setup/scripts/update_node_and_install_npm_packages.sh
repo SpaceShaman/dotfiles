119f871ba38e3c47318863b2402bd69dbbf0c9cb
SpaceShaman <spaceshaman@tuta.io>
2025-09-05 10:08:28 +0200

    🔧 (update_node_and_install_npm_packages.sh): add sudo to npm global installs to ensure proper permissions

--- setup/scripts/update_node_and_install_npm_packages.sh
+++ setup/scripts/update_node_and_install_npm_packages.sh
@@ -1,9 +1,9 @@
 if ask "Do you want to update Node and install npm packages?"; then
-  npm install -g n
+  sudo npm install -g n
   n lts
   n prune
-  npm install -g npm@latest
+  sudo npm install -g npm@latest
 
-  npm install -g opencommit markdownlint-cli2
+  sudo npm install -g opencommit markdownlint-cli2
   success "Node and npm packages updated!"
 fi

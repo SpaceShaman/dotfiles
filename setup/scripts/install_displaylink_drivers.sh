8b7391584a40abab2d14fe8191de5d4bf23ce73f
SpaceShaman <spaceshaman@tuta.io>
2025-09-05 12:59:50 +0200

    🐛 (install_displaylink_drivers.sh): fix directory cleanup by adding a step to navigate back before removing the cloned repository

--- setup/scripts/install_displaylink_drivers.sh
+++ setup/scripts/install_displaylink_drivers.sh
@@ -3,6 +3,7 @@ if ask "Do you want to install DisplayLink drivers?"; then
   git clone https://github.com/AdnanHodzic/displaylink-debian.git
   cd displaylink-debian
   sudo ./displaylink-debian.sh
+  cd ..
   rm -rf displaylink-debian
   success "DisplayLink USB HDMI drivers installed!"
 fi

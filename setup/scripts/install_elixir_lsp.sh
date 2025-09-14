f611a51a93d482563cc18a4179664afc8c965171
SpaceShaman <spaceshaman@tuta.io>
2025-09-14 17:56:21 +0200

    ✨ (install_elixir_lsp.sh): add installation steps for 'just' and 'ZIG' to support Elixir LSP setup ♻️ (install_elixir_lsp.sh): refactor Elixir LSP installation to use 'just' for dependency management

--- setup/scripts/install_elixir_lsp.sh
+++ setup/scripts/install_elixir_lsp.sh
@@ -1,12 +1,23 @@
 if ask "Do you want to install Elixir LSP?"; then
+  info "Installing just"
+  asdf plugin add just
+  asdf install just latest
+  asdf set --home just latest
+  success "Just installed!"
+  info "Installing ZIG"
+  wget https://ziglang.org/download/0.14.1/zig-x86_64-linux-0.14.1.tar.xz
+  tar -xf zig-x86_64-linux-0.14.1.tar.xz
+  sudo mv zig-x86_64-linux-0.14.1 /home/ton618/.local/share/zig
+  ln -sf $HOME/.local/share/zig/zig /home/ton618/.local/bin/zig
+  rm -rf zig-x86_64-linux-0.14.1 zig-x86_64-linux-0.14.1.tar.xz
+  success "ZIG installed!"
   info "Installing Elixir LSP..."
-  LATEST_URL=$(curl -s https://api.github.com/repos/elixir-lsp/elixir-ls/releases/latest \
-    | jq -r '.assets[] | select(.name | test("elixir-ls.*\\.zip$")).browser_download_url')
-  curl -L -o elixir-ls.zip $LATEST_URL
-
-  sudo rm -rf /home/ton618/.local/share/elixir-lsp/elixir-ls
-  sudo mkdir -p /home/ton618/.local/share/elixir-lsp
-  sudo unzip elixir-ls.zip -d /home/ton618/.local/share/elixir-lsp/elixir-ls
-  rm elixir-ls.zip
+  git clone git@github.com:elixir-lang/expert.git
+  cd expert
+  just deps forge
+  just deps engine
+  just deps expert
+  just install
+  rm -rf expert
   success "Elixir LSP installed!"
 fi

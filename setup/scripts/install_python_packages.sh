856010cde0b46a904a94f2beb1807460d05a086a
SpaceShaman <spaceshaman@tuta.io>
2025-11-27 13:49:10 +0100

    🔧 (mc/ini): adjust left panel size for better layout 🔧 (setup): remove install_python_packages.sh script from setup process

--- setup/scripts/install_python_packages.sh
+++
@@ -1,6 +0,0 @@
-if ask "Do you want to install python packages?"; then
-  info "Installing python packages..."
-  python3 -m pip config set global.break-system-packages true
-  pip install shell_gpt ptpython
-  success "Python packages installed!"
-fi

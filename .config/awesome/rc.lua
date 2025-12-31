76c25f44c120b96f232ceb90537ca30bafe2677a
SpaceShaman <spaceshaman@tuta.io>
2025-12-31 07:24:19 +0100

    🔧 (alacritty.toml, alacritty.yml): increase font size from 15 to 16 for better readability
    🔧 (rc.lua): add function to center mouse on primary screen for improved user experience
    
    🔧 (mc/ini): adjust left panel size from 106 to 39 for better layout management

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -368,3 +368,13 @@ awful.spawn.with_shell 'xset +dpms && xset s on && xset s 300'
 
 -- Setup displays
 awful.spawn.with_shell 'bash /home/ton618/.scripts/screen.sh -r'
+
+local function center_mouse()
+  local total_width = 0
+  for s in screen do
+    total_width = total_width + s.geometry.width
+  end
+  root.fake_input('motion_notify', false, total_width / 2, screen.primary.geometry.height / 2)
+end
+
+center_mouse()

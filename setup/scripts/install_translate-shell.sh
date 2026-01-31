855405b2f76d089ae63ed73c2eaed361e04a3050
SpaceShaman <spaceshaman@tuta.io>
2026-01-31 08:54:27 +0100

    🔧 (install_translate-shell.sh): add gawk installation to ensure translate-shell dependencies are met

--- setup/scripts/install_translate-shell.sh
+++ setup/scripts/install_translate-shell.sh
@@ -3,5 +3,6 @@ if ask "Do you want to install translate-shell?"; then
   wget git.io/trans
   chmod +x trans
   sudo mv trans /usr/local/bin/trans
+  sudo apt install -y gawk
   success "translate-shell installed!"
 fi

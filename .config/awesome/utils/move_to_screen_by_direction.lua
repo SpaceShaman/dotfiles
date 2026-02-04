d7fd315ff3af150c3958c2a84f8f82365599befe
SpaceShaman <spaceshaman@tuta.io>
2026-02-04 08:36:12 +0100

    🔧 (utils/move_to_screen_by_direction.lua): change string quotes to single quotes for consistency 🔧 (screen.sh): swap SCREEN_PRIMARY and SCREEN_LEFT values to correct screen configuration

--- .config/awesome/utils/move_to_screen_by_direction.lua
+++ .config/awesome/utils/move_to_screen_by_direction.lua
@@ -1,28 +1,28 @@
-local awful = require("awful")
+local awful = require 'awful'
 
 return function(direction, c)
-    local screen_left = "DVI-I-2-2"
-    local screen_right = "DVI-I-1-1"
-    local screen_bottom = "eDP-1"
+  local screen_left = 'DVI-I-2-2'
+  local screen_right = 'DVI-I-1-1'
+  local screen_bottom = 'eDP-1'
 
-    local current_screen = awful.screen.focused()
-    for k, v in pairs(current_screen.outputs) do
-        if direction == "left" then
-            if k == screen_right or k == screen_bottom then
-                c:move_to_screen(screen_left)
-            end
-        elseif direction == "right" then
-            if k == screen_left or k == screen_bottom then
-                c:move_to_screen(screen_right)
-            end
-        elseif direction == "up" then
-            if k == screen_bottom then
-                c:move_to_screen(screen_right)
-            end
-        elseif direction == "down" then
-            if k == screen_right or k == screen_left then
-                c:move_to_screen(screen_bottom)
-            end
-        end
+  local current_screen = awful.screen.focused()
+  for k, v in pairs(current_screen.outputs) do
+    if direction == 'left' then
+      if k == screen_right or k == screen_bottom then
+        c:move_to_screen(screen_left)
+      end
+    elseif direction == 'right' then
+      if k == screen_left or k == screen_bottom then
+        c:move_to_screen(screen_right)
+      end
+    elseif direction == 'up' then
+      if k == screen_bottom then
+        c:move_to_screen(screen_right)
+      end
+    elseif direction == 'down' then
+      if k == screen_right or k == screen_left then
+        c:move_to_screen(screen_bottom)
+      end
     end
+  end
 end

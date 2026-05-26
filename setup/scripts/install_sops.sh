20cbc6cf721b24716708d91823369a9afb5f1844
SpaceShaman <spaceshaman@tuta.io>
2026-05-26 10:50:50 +0200

    🔧 (install_sops.sh): add installation of 'age' dependency for SOPS

--- setup/scripts/install_sops.sh
+++ setup/scripts/install_sops.sh
@@ -1,5 +1,6 @@
 if ask "Do you want to install SOPS?"; then
   info "Installing sops..."
+  sudo apt install -y age
   curl -LO https://github.com/getsops/sops/releases/download/v3.13.1/sops-v3.13.1.linux.amd64
   mv sops-v3.13.1.linux.amd64 ~/.local/bin/sops
   chmod +x ~/.local/bin/sops
